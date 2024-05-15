a70dc19be5fcf5e056050d4f4bd25427f746abc5
vaishnavi <2020BTEIT00031@walchandsangli.ac.in>
2024-05-15 21:00:09 +0530

    update file

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-// import Upload from './components/upload.js'
+import Upload from './components/upload.js'
 import Graph from './components/graph.js'
-import Table from './components/Table.js'
+// import Table from './components/Table.js'
 
 const Router = () => {
   return (
     <>
     <Routes>
-        <Route path="/" element={<Table />} />
+        <Route path="/" element={<Upload />} />
         <Route path="/graph" element={<Graph />} />
     </Routes>
     </>
